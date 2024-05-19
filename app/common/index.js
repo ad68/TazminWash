75f5405bcfe22e7324a428d6da58321c28c036d4
ali dargahi <alidargahi1368@gmail.com>
2024-05-19 11:59:38 +0330

    .

---
+++ app/common/index.js
@@ -0,0 +1,32 @@
+import Select from "./inputs/MySelect";
+import AsyncSelect from "./inputs/AsyncSelect";
+import TextBox from "./inputs/Textbox";
+import Number from "./inputs/Number";
+import Modal from "./Modal";
+import LoadingSpinner from "./LoadingSpinner";
+import Button from "./Button";
+import RangePicker from "./RangePicker";
+import ConfirmModal from "./ConfirmModal";
+import QuestionModal from "./QuestionModal";
+import DeleteModal from "./DeleteModal";
+import Notify from "./Notify";
+import ComponentLoading from "./ComponentLoading";
+import ErrorMessage from "./Message/ErrorMessage";
+export {
+  Select,
+  AsyncSelect,
+  TextBox,
+  Number,
+  LoadingSpinner,
+  Button,
+  RangePicker,
+  Modal,
+  CarLicensePlate,
+  CycleLicensePlate,
+  ConfirmModal,
+  QuestionModal,
+  DeleteModal,
+  Notify,
+  ComponentLoading,
+  ErrorMessage,
+};
