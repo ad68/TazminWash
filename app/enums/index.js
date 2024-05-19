238ec599047562a4a0ad6c7cc65564bdb5477cb0
ali dargahi <alidargahi1368@gmail.com>
2024-05-19 12:01:25 +0330

    .

---
+++ app/enums/index.js
@@ -0,0 +1,12 @@
+export const NotifyType = Object.freeze({
+  SUCCESS: "success",
+  ERROR: "error",
+});
+export const NotifyMessage = Object.freeze({
+  SUCCESS_ACTION: "عملیات مورد نظر با موفقیت انجام شد",
+  GLOBAL_ERROR: "خطایی رخ داده است",
+});
+export const Regex = Object.freeze({
+  MOBILE: new RegExp(/^[0][9][0-9][0-9]{8}$/),
+  NATIONAL_CODE: new RegExp(/^([0-9]){10}$/),
+});
