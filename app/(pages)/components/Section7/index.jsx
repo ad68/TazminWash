4875c2f4d388819f96352e3eeb881ee1f2b3f6a6
Mina Yousefnezhad <93423309+Minay98@users.noreply.github.com>
2024-05-20 12:35:11 +0330

    Section7

--- app/(pages)/components/Section7/index.jsx
+++ app/(pages)/components/Section7/index.jsx
@@ -1,18 +1,13 @@
 "use client";
 import React from "react";
-import Section1 from "./components/section1";
-import Section2 from "./components/Section2";
-import Section3 from "./components/Section3";
-import Section5 from "./components/Section5";
-import Section6 from "./components/Section6";
-import Section7 from "./components/Section7";
-//
+import Image from "next/image";
+import Dot from "./components/Dot";
 // ────────────────────────────────────────────────────────── I ──────────
 //   :::::: C O M P O N E N T : :  :   :    :     :        :          :
 // ────────────────────────────────────────────────────────────────────
 //
 
-export default function page() {
+export default function Index() {
   // ─── Global Variable ────────────────────────────────────────────────────────────
 
   // ─── States ─────────────────────────────────────────────────────────────────────
@@ -20,20 +15,51 @@ export default function page() {
   // ─── Life Cycle ─────────────────────────────────────────────────────────────────
 
   // ─── Functions ──────────────────────────────────────────────────────────────────
-  //
 
+  //
   // ──────────────────────────────────────────────────── I ──────────
   //   :::::: R E N D E R : :  :   :    :     :        :          :
   // ──────────────────────────────────────────────────────────────
   //
   return (
     <>
-      <Section1 />
-      <Section2 />
-      <Section3 />
-      <Section5 />
-      <Section6 />
-      <Section7 />
+      <section className="my-[120px]">
+        <header className="flex justify-center gap-[22px]">
+          <Image
+            src="/images/icons/Frame 1261154674.svg"
+            width={32}
+            height={32}
+            alt=""
+          />
+          <h1 className="text-[32px] font-extrabold">نظرات مشتریان</h1>
+          <Image
+            src="/images/icons/Frame 1261154674.svg"
+            width={32}
+            height={32}
+            alt=""
+          />
+        </header>
+        <section className="mt-6 grid h-[580px] w-full justify-items-center bg-[url('/images/BG.png')] bg-cover py-[80px]">
+          <Image src="/images/icons/Vector.svg" width={18} height={15} alt="" />
+          <h3 className="w-[856px] text-center text-2xl font-bold leading-[49px]">
+            با سلام و عرض ادب ، من از خدمات و نتیجه نهایی کار بسیار راضی بودم
+            همچنین بسیار کادر مودب و آن تایمی داشتند .
+          </h3>
+          <Image
+            src="/images/icons/Vectort.svg"
+            width={18}
+            height={15}
+            alt=""
+          />
+          <span className="text-[16px] font-[400px]">هاشم زاده | تهران</span>
+          <section className="flex gap-6">
+            <Dot />
+            <Dot />
+            <Dot />
+            <Dot />
+          </section>
+        </section>
+      </section>
     </>
   );
 }
