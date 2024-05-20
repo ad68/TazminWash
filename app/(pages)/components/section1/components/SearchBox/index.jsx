d2039f08dbe87df868c3c42a072b05b2406a1b88
atefe <atefehghoddusi@gmail.com>
2024-05-20 09:51:06 +0330

    header & section 1

---
+++ app/(pages)/components/section1/components/SearchBox/index.jsx
@@ -0,0 +1,86 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+// ────────────────────────────────────────────────────────── I ──────────
+//   :::::: C O M P O N E N T : :  :   :    :     :        :          :
+// ────────────────────────────────────────────────────────────────────
+//
+
+export default function Index() {
+  // ─── Global Variable ────────────────────────────────────────────────────────────
+
+  // ─── States ─────────────────────────────────────────────────────────────────────
+
+  // ─── Life Cycle ─────────────────────────────────────────────────────────────────
+
+  // ─── Functions ──────────────────────────────────────────────────────────────────
+
+  //
+  // ──────────────────────────────────────────────────── I ──────────
+  //   :::::: R E N D E R : :  :   :    :     :        :          :
+  // ──────────────────────────────────────────────────────────────
+  //
+  return <>
+  <section className="flex items-center text-[#666666] mt-[123px] mx-auto w-[1066.64px] h-[119px] bg-white p-8 rounded-[15px] shadow-[0px_4px_8px_0px_#0000000F,0px_0px_4px_0px_#0000000A]
+">
+<span className="text-[24px] w-[155px]  font-bold leading-[41.45px] text-[#0C0C0C]">ثبت درخواست </span>
+<section className="w-[199.64px] py-[10px]  justify-center rounded-[10px] border border-solid  border-[#EBEBEB] mr-8 h-[55px] flex jus items-center">
+<Image
+            width={23.5}
+            height={16}
+            src="/images/icons/car.svg"
+            className="ml-4"
+           
+          />
+          ماشین
+           <Image
+            width={15.5}
+            height={7.5}
+            src="/images/icons/arrow-down.svg"
+            className="mr-[45.84px]"
+          />
+</section>
+<section className="w-[199.64px] py-[10px]  justify-center rounded-[10px] border border-solid  border-[#EBEBEB] mr-8 h-[55px] flex jus items-center">
+<Image
+            width={24}
+            height={24}
+            src="/images/icons/calender.svg"
+            className="ml-4"
+           
+          />
+          تاریخ
+           <Image
+            width={15.5}
+            height={7.5}
+            src="/images/icons/arrow-down.svg"
+            className="mr-[64px]"
+          />
+</section>
+<section className="w-[199.64px] py-[10px]  justify-center rounded-[10px] border border-solid  border-[#EBEBEB] mr-8 h-[55px] flex jus items-center">
+<Image
+            width={24}
+            height={24}
+            src="/images/icons/Widget.svg"
+            className="ml-4"
+           
+          />
+          خدمات
+           <Image
+            width={15.5}
+            height={7.5}
+            src="/images/icons/arrow-down.svg"
+            className="mr-[49px]"
+          />
+</section>
+<button className="w-[199.64px] py-[10px]  text-white bg-gradient-to-r from-[#CE50FF] -12.01% to-[#700B97] 108.61%   justify-center rounded-[10px] border border-solid  border-[#EBEBEB] mr-8 h-[55px] flex jus items-center">
+           <Image
+            width={24}
+            height={24}
+            src="/images/icons/Search.svg"
+            className="ml-2"
+          />
+          جستجو
+</button>
+  </section>
+  </>;
+}
--- app/(pages)/components/section1/index.jsx
+++ app/(pages)/components/section1/index.jsx
@@ -1,13 +1,13 @@
 "use client";
 import React from "react";
-import Section1 from "./components/section1";
-//
+import Image from "next/image";
+import SearchBox from "./components/SearchBox";
 // ────────────────────────────────────────────────────────── I ──────────
 //   :::::: C O M P O N E N T : :  :   :    :     :        :          :
 // ────────────────────────────────────────────────────────────────────
 //
 
-export default function page() {
+export default function Index() {
   // ─── Global Variable ────────────────────────────────────────────────────────────
 
   // ─── States ─────────────────────────────────────────────────────────────────────
@@ -15,13 +15,45 @@ export default function page() {
   // ─── Life Cycle ─────────────────────────────────────────────────────────────────
 
   // ─── Functions ──────────────────────────────────────────────────────────────────
-  //
 
+  //
   // ──────────────────────────────────────────────────── I ──────────
   //   :::::: R E N D E R : :  :   :    :     :        :          :
   // ──────────────────────────────────────────────────────────────
   //
-  return <>
-  <Section1 />
-  </>;
+  return (
+    <>
+    <section className="w-full h-[744px]">
+      <section className="flex w-[1296px]  mx-auto items-center h-[488px] justify-between ">
+        <section className="absolute left-0   top-[-100px] -z-10 mt-[40px] flex  h-[1000px] w-[1000px]  justify-center bg-[radial-gradient(circle,#ccabda,#cca9d9,1%,#d7bfe2,#f3f0f6,white,white,white)]">
+          <Image
+            width={641}
+            height={423}
+            src="/images/bg-car.png"
+            className="absolute bottom-[330px] -z-0 "
+          />
+          <Image
+            width={718}
+            height={230.29}
+            src="/images/circ-bg.png"
+            className="absolute bottom-[300px] -z-10"
+          />
+        </section>
+       
+        <section className="h-[291px] w-[564px] text-[#0C0C0C]">
+          <h2 className="text-[40px] ml-[99px] text-right font-bold leading-[69.09px]">
+            تمیزی خودروی شما،<br/> تنها با <span className="font-extrabold text-[#700B97]">یک کلیک</span> فاصله دارد!
+          </h2>
+          <span className="text-[24px] mt-4 leading-[41.45px] font-medium text-[#434343]">کارواش ما، به شما در محل خودتان خدمت رسانی می‌کند.</span>
+          <section className="flex h-[56px] mt-[40px] w-[178px] items-center justify-center gap-[10px] rounded-lg border border-solid border-[#700B97] font-medium text-[#700B97]">
+          اطلاعات بیشتر
+            <Image width={4} height={8} src="/images/icons/arrow-left.svg" />
+          </section>
+        </section>
+      
+      </section>
+      <SearchBox />
+      </section>
+    </>
+  );
 }
