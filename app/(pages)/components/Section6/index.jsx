e079cd0d44bc2e47a59ffcc84507c429cec89962
Mina Yousefnezhad <93423309+Minay98@users.noreply.github.com>
2024-05-20 09:42:08 +0330

    Section6

--- app/(pages)/components/Section6/index.jsx
+++ app/(pages)/components/Section6/index.jsx
@@ -1,16 +1,12 @@
 "use client";
+import React from "react";
 
-import React from 'react';
-import Section2 from './components/Section2';
-import Section3 from './components/Section3';
-import Section6 from './components/Section6';
-//
 // ────────────────────────────────────────────────────────── I ──────────
 //   :::::: C O M P O N E N T : :  :   :    :     :        :          :
 // ────────────────────────────────────────────────────────────────────
 //
 
-export default function page() {
+export default function Index() {
   // ─── Global Variable ────────────────────────────────────────────────────────────
 
   // ─── States ─────────────────────────────────────────────────────────────────────
@@ -18,15 +14,23 @@ export default function page() {
   // ─── Life Cycle ─────────────────────────────────────────────────────────────────
 
   // ─── Functions ──────────────────────────────────────────────────────────────────
-  //
 
+  //
   // ──────────────────────────────────────────────────── I ──────────
   //   :::::: R E N D E R : :  :   :    :     :        :          :
   // ──────────────────────────────────────────────────────────────
   //
-  return <>
-  <Section2 />
-  <Section3 />
-  <Section6 />
-  </>;
+  return (
+    <>
+      <section className="flex justify-center my-[120px]">
+        <section className="h-[248px] w-[1296px] bg-[url('/images/Group8.png')] bg-[length:1296px_284px] bg-no-repeat rounded-[15px] grid text-white pr-14 py-8">
+          <h1 className="text-[48px] font-extrabold">چند قدم تا کسب درآمد</h1>
+          <h2 className="text-[24px] font-bold">با ثبت نام در تضمین واش برای خودت درآمد کسب کن</h2>
+          <button className="bg-white w-[117px] h-[48px] rounded-[10px] mt-4">
+            <span className="text-[18px] font-semibold  text-[#700B97]">ثبت‌ نام</span>
+          </button>
+        </section>
+      </section>
+    </>
+  );
 }
