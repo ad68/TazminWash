f1072a8a2a1c66d1c0e70d39a3161be9f9765528
ali dargahi <alidargahi1368@gmail.com>
2024-05-20 10:11:22 +0330

    .

--- app/(pages)/page.js
+++ app/(pages)/page.js
@@ -1,11 +1,9 @@
 "use client";
 import React from "react";
 import Section1 from "./components/section1";
-
-import React from 'react';
-import Section2 from './components/Section2';
-import Section3 from './components/Section3';
-import Section6 from './components/Section6';
+import Section2 from "./components/Section2";
+import Section3 from "./components/Section3";
+import Section6 from "./components/Section6";
 //
 // ────────────────────────────────────────────────────────── I ──────────
 //   :::::: C O M P O N E N T : :  :   :    :     :        :          :
@@ -26,10 +24,12 @@ export default function page() {
   //   :::::: R E N D E R : :  :   :    :     :        :          :
   // ──────────────────────────────────────────────────────────────
   //
-  return <>
-  <Section1 />
-  <Section2 />
-  <Section3 />
-  <Section6 />
-  </>;
+  return (
+    <>
+      <Section1 />
+      <Section2 />
+      <Section3 />
+      <Section6 />
+    </>
+  );
 }
