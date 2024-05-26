d02278b66d9683346287d9bb364078192e7e6aa6
atefe <atefehghoddusi@gmail.com>
2024-05-26 12:31:08 +0330

    footer design & responsive darkmode

--- app/(pages)/page.js
+++ app/(pages)/page.js
@@ -36,8 +36,8 @@ export default function page() {
       <Section3 />
       <Section4 />
       <Section5 />
-      <Section6 />
-      <Section7 />
+      {/* <Section6 /> */}
+      {/* <Section7 /> */}
     </>
   );
 }
