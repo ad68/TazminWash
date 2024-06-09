7b567d821ae9fd5dd8ebe7834548d3d47ce5d2cd
ali dargahi <alidargahi1368@gmail.com>
2024-06-09 16:43:41 +0330

    .

--- app/(pages)/order/components/Address/Map/components/Map.jsx
+++ app/(pages)/order/components/Address/Map/components/Map.jsx
@@ -6,17 +6,15 @@ import React, {
   useCallback,
 } from "react";
 
-import { MapContainer, Marker, TileLayer,Popup } from "react-leaflet";
+import { MapContainer, Marker, TileLayer, Popup } from "react-leaflet";
 import Image from "next/image";
-
 import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
-const center = [35.7249, -308.5904 ];
+const center = [35.7249, -308.5904];
 const zoom = 16;
 function DisplayPosition({ map }) {
   const [position, setPosition] = useState(() => map.getCenter());
-
   const onClick = useCallback(() => {
     map.setView(center, zoom);
   }, [map]);
@@ -45,21 +43,19 @@ export default function MyMap() {
   const displayMap = useMemo(
     () => (
       <section className="relative">
- 
-      <MapContainer
-        center={center}
-        zoom={zoom}
-        scrollWheelZoom={true}
-        ref={setMap}
-      >
+        <MapContainer
+          center={center}
+          zoom={zoom}
+          scrollWheelZoom={true}
+          ref={setMap}
+        >
+          <Image alt="" src='/images/icons/pin.png' width={40} height={50} className="z-[1000] absolute top-[50%] right-[50%] translate-x-[50%] translate-y-[-50%]" />
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
 
-<Image alt="" src='/images/icons/pin.png' width={40} height={50} className="z-[1000] absolute top-[50%] right-[50%] translate-x-[50%] translate-y-[-50%]"/>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-      
-      </MapContainer>
+        </MapContainer>
       </section>
     ),
     [],
